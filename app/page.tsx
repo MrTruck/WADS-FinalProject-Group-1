45e88cabc80d58290965bc3dbcb7147805104707
Sheva123456 <imanuel.sheva.simanjuntak@gmail.com>
2026-05-28 18:30:33 +0700

    Updated calendar with weekly and daily view. Added ai setup and docker setup.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         
         <div className="mt-8 w-full flex items-center justify-center">
-          <Calendar />
+          <Calendar/>
         </div>
       </main>
     </div>
